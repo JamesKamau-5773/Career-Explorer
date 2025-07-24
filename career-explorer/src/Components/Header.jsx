7d1aa8a786c887e6c23c4b3276cf02ded326e8f0
cynthia <cynthialairumbe@gmail.com>
2025-07-24 22:04:47 +0300

    feat:Header

---
+++ career-explorer/src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <header className='header'>
+      <h1>CAREER EXPLORER</h1>
+      <div className="header-content">
+        <nav className="nav">
+        
