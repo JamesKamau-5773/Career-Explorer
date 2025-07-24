64511e0f328bdef7a8e1b2ccabaee1bb41de9f30
roy9 <roymurimi527@gmail.com>
2025-07-24 20:58:32 +0300

    Footer

---
+++ career-explorer/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer style={{ marginTop: 'auto', padding: '1rem' }}>
+      <p>&copy; 2024 Career Explorer. All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
