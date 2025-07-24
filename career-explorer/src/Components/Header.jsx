39392d5ecef2fac550553bed9b4aa149da064821
cynthia <cynthialairumbe@gmail.com>
2025-07-24 22:11:25 +0300

    Add routes

--- career-explorer/src/Components/Header.jsx
+++ career-explorer/src/Components/Header.jsx
@@ -8,3 +8,45 @@ import AllCareers from './components/AllCareers'
 import FilteredCareers from './components/FilteredCareers'
 import CareerDetail from './components/CareerDetail'
 import Contacts from './components/Contacts'
+
+function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchFilters, setSearchFilters] = useState({});
+
+  return (
+    <Router>
+      <div className="app">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route 
+              path="/search" 
+              element={
+                <Search 
+                  setSearchResults={setSearchResults}
+                  setSearchFilters={setSearchFilters}
+                />
+              } 
+            />
+            <Route path="/all-careers" element={<AllCareers />} />
+            <Route 
+              path="/filtered-careers" 
+              element={
+                <FilteredCareers 
+                  searchResults={searchResults}
+                  searchFilters={searchFilters}
+                />
+              } 
+            />
+            <Route path="/career/:id" element={<CareerDetail />} />
+            <Route path="/contacts" element={<Contacts />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  )
+}
+
+export default App
