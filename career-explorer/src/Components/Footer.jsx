3598c60c1b08a8e7825bb19e4053af0b6d62ce23
roy9 <roymurimi527@gmail.com>
2025-07-24 21:03:37 +0300

    Enhance Footer component with centered text and top border styling

--- career-explorer/src/Components/Footer.jsx
+++ career-explorer/src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer style={{ marginTop: 'auto', padding: '1rem' }}>
+    <footer style={{ marginTop: 'auto', padding: '1rem', textAlign: 'center', borderTop: '1px solid #333' }}>
       <p>&copy; 2024 Career Explorer. All rights reserved.</p>
     </footer>
   )
