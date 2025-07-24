3d4a5bd439fae3eda0fa2de2cde3d963e67db9b9
cynthia <cynthialairumbe@gmail.com>
2025-07-24 22:07:23 +0300

    add links

--- career-explorer/src/Components/Header.jsx
+++ career-explorer/src/Components/Header.jsx
@@ -7,4 +7,16 @@ function Header() {
       <h1>CAREER EXPLORER</h1>
       <div className="header-content">
         <nav className="nav">
-        
+          <Link to="/" className="link"> Home</Link>
+          <Link to="/search" className="link">Search</Link>
+          <Link to="/all-careers" className="link">All Careers</Link>
+          <Link to="/filtered-careers" className="link"> Filtered Results</Link>
+          <Link to="/contacts" className="link"> Contact</Link>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header
+
